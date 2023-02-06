ec8c93d9b7e0cc0872f215f13a25d93dd9686518
anupal nath <anupalnath70@gmail.com>
2023-02-07 00:30:10 +0530

    merged with passauth

--- models/db_model.js
+++ models/db_model.js
@@ -2,12 +2,12 @@ const mongoose = require("mongoose");
 
 // user Schema
 const userSchema = new mongoose.Schema({
-
+  username: { type: String, required: [true, "please provide a user name"],trim:true },
 
   email: {
     type: String,
     required: [true, "email is required"],
-    unique:[true,"user already exists"]
+    unique: [true, "user already exists"],
   },
   password: { type: String, required: [true, "fill in a password"] },
 });
