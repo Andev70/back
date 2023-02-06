5f8312c4434fd4c21b5daa287115420efbda7a4e
anupal nath <anupalnath70@gmail.com>
2023-02-06 15:24:56 +0530

    password auth completed

---
+++ public/dashboard/dashboard.js
@@ -0,0 +1,51 @@
+const authToken = sessionStorage.getItem("token");
+const caption = document.querySelector(".caption");
+const btn = document.querySelector(".btn");
+
+const getUser = async () => {
+  try {
+    const res = await fetch("/api/v1/users/user", {
+      method: "GET",
+      headers: { authorization: authToken },
+    });
+    const data = res.json();
+    return data;
+  } catch (e) {
+    console.log(e);
+  }
+};
+getUser().then((res) => console.log(res));
+
+const postsGet = async () => {
+  try {
+    const res = await fetch("/api/v1/posts", {
+      method: "GET",
+      headers: { authorization: authToken },
+    });
+    const data = res.json();
+    return data;
+  } catch (e) {
+    console.log(e);
+  }
+};
+postsGet().then((res) => console.log(res));
+
+const createPost = async () => {
+  try {
+    const res = await fetch("/api/v1/posts", {
+      method: "GET",
+      headers:{authorization:authToken},
+      body: JSON.stringify({ caption: caption.value, likes: 30 }),
+    });
+    const data = res.json();
+    return data;
+  } catch (e) {
+    console.log(e);
+  }
+};
+
+btn.addEventListener("click", () => {
+  createPost().then((res) => {
+    console.log(res);
+  });
+});
